dbacbd3d2fc3874a3bb3290e4672753d99e6091d
tuhin-thinks <tuhinmitra190221@gmail.com>
2023-07-02 01:43:18 +0530

    Added controller to scan resume file.

--- controllers/v1/resumeAnalyzeControllers.js
+++ controllers/v1/resumeAnalyzeControllers.js
@@ -18,11 +18,25 @@ const uploadResumeFile = async (req, res) => {
         size: req.file.size,
         userId: req.user._id,
     });
-    await tempFile.save();
+    const result = await tempFile.save();
+    req.cookies.tempFileId = result._id; // TODO: check if this works
 
     return res.status(200).json({ message: 'File uploaded successfully' });
 };
 
+const analyzeResumeFile = async (req, res) => {
+    const tempFileId = req.cookies.tempFileId;
+    const tempFile = await TempFileModel.findById(tempFileId);
+    if (!tempFile) {
+        return res.status(400).json({ message: 'File not found' });
+    }
+
+    // TODO: write code to analyze the resume file
+
+    return res.status(200).json({ message: 'File analyzed successfully' });
+};
+
 module.exports = {
     uploadResumeFile,
+    analyzeResumeFile,
 };
