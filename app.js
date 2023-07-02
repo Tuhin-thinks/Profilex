da23c8941e0570888dfcc578db7aa48140223cf2
tuhin-thinks <tuhinmitra190221@gmail.com>
2023-07-02 17:35:25 +0530

    Trying with localhost as cors origin

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: '*',
+        origin: 'localhost:3000',
         credentials: true,
     })
 );
