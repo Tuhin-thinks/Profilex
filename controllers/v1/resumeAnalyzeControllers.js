28e1a634a4964280f7e6124ebf3ce421146fb5f2
tuhin-thinks <tuhinmitra190221@gmail.com>
2023-07-02 11:00:07 +0530

    - Removed commented out code.

--- controllers/v1/resumeAnalyzeControllers.js
+++ controllers/v1/resumeAnalyzeControllers.js
@@ -6,13 +6,6 @@ const { User } = require('../../models/UserModel');
  * Controller to upload resume file
  */
 const uploadResumeFile = async (req, res) => {
-    // await fileUpload(req, res, (err) => {
-    //     if (err) {
-    //         res.status(400).json({ message: 'Error uploading file' });
-    //     } else {
-    //         res.status(200).json({ message: 'File uploaded successfully' });
-    //     }
-    // });
     const userId =
         req.user?._id ||
         (await User.findOne({ email: 'guest-user@mail.com' }))._id;
